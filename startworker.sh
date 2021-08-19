019a4cec4e2c8f3fdeb47d13ee217c585aeefc13
mpramod2899 <mpramod@gmail.com>
2021-08-18 22:19:08 -0400

    Delete startworker.sh
    del

--- startworker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-/opt/kafkaconnector/rabitmq-connector/confluent-6.2.0/bin/connect-distributed -daemon /opt/kafkaconnector/rabitmq-connector/config/connect-tch.properties
-
-echo "Verify the logs in the Following location"
-echo "/opt/kafkaconnector/rabitmq-connector/confluent-6.2.0/logs/connect.log"
-echo "/opt/kafkaconnector/rabitmq-connector/confluent-6.2.0/logs/connectDistributed.out"
