c836f2d3944a300f270c98fd0a11aafe840b89fe
mpramod2899 <mpramod@gmail.com>
2021-08-18 22:19:32 -0400

    Delete submit_connector.sh
    del

--- submit_connector.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-HEADER="Content-Type: application/json"
-
-curl -k -X DELETE -H "${HEADER}"  https://localhost:8083/connectors/RabbitMQ || exit 1
-curl -k -X POST -H "${HEADER}" --data "@${1}"  https://localhost:8083/connectors || exit 1
-
