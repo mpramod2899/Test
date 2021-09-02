c1b1c44a2397b075c60d65247b2bdc3ae9cac515
mpramod2899 <mpramod@gmail.com>
2021-09-02 13:55:08 -0400

    Delete create_topics.sh
    delete

--- create_topics.sh
+++
@@ -1,5 +0,0 @@
-/opt/kafkaconnector/rabitmq-connector/confluent-6.2.0/bin/kafka-topics --zookeeper 127.0.0.1:2181 --create --topic confluent.connect-offsets-vm --partitions 1 --replication-factor 1 --config cleanup.policy=compact
-/opt/kafkaconnector/rabitmq-connector/confluent-6.2.0/bin/kafka-topics --zookeeper 127.0.0.1:2181 --create --topic confluent.connect-status-vm --partitions 1 --replication-factor 1 --config cleanup.policy=compact
-/opt/kafkaconnector/rabitmq-connector/confluent-6.2.0/bin/kafka-topics --zookeeper 127.0.0.1:2181 --create --topic confluent.connect-configs-vm --partitions 1 --replication-factor 1 --config cleanup.policy=compact
-/opt/kafkaconnector/rabitmq-connector/confluent-6.2.0/bin/kafka-topics --zookeeper 127.0.0.1:2181 --create --topic test-end-to-end --partitions 1 --replication-factor 1 
-/opt/kafkaconnector/rabitmq-connector/confluent-6.2.0/bin/kafka-topics --zookeeper 127.0.0.1:2181 --create --topic Test_Kafka --partitions 1 --replication-factor 1
